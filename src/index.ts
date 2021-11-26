47141b83c95f9b33a9253f1d2ae02b71b924cf82
Adriano Oliveira <60880274+adrianlivr@users.noreply.github.com>
2021-11-26 15:45:10 -0300

    :card_index: Export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { AesGcmModule } from './aes-gcm/aes-gcm.module';
 export { AesGcmService } from './aes-gcm/aes-gcm.service';
+export {
+  AesGcmModuleAsyncOptions,
+  AesGcmModuleOptions,
+  AesGcmModuleOptionsFactory,
+} from './aes-gcm/aes-gcm.interface';
